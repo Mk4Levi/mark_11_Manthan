1293428bf74071d8bb10f26b75bb92c66a583335
Mk4Levi <120794789+Mk4Levi@users.noreply.github.com>
2023-01-09 20:30:03 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#calculate-number");
+const displayMessage = document.querySelector("#output-container");
+
+const calculateSum = (date) => {
+  let sum = 0;
+  date = date.replaceAll("-", "");
+  for (let digit of date) {
+    sum = sum + Number(digit);
+  }
+  return sum;
+};
+
+const checkIsNumberLucky = (sumOfDate, numberToCheck) => {
+  console.log(sumOfDate, numberToCheck);
+  if (sumOfDate % numberToCheck === 0) {
+    return showMessage(`${numberToCheck} is a lucky number!! 🥳 🥳 🥳 `);
+  }
+  showMessage(`${numberToCheck} is not that lucky 😕`);
+};
+
+const showMessage = (message) => {
+  displayMessage.innerText = message;
+};
+
+checkButton.addEventListener("click", () => {
+  const date = dateOfBirth.value;
+  const numberToCheck = luckyNumber.value;
+  if (date && numberToCheck) {
+    const sumOfDate = calculateSum(date);
+    checkIsNumberLucky(sumOfDate, numberToCheck);
+  } else {
+    showMessage("Please enter both the fields");
+  }
+});
